6a50650a449a3e5262f8cb8ff9aace7545f146db
Gavin Blackwell <99514717+GavinB5@users.noreply.github.com>
2024-02-05 12:16:28 -0600

    Add files via upload

---
+++ game.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class students {
+
+ public static void main(String[] args){
+     Scanner sc = new Scanner(System.in);
+     //Setup
+     string [] weapons = new string [5];
+       weapons[0] = "Sword";
+       weapons[1] = "Bow";
+       weapons[2] = "Axe";
+       weapons[3] = "Trident";
+       weapons[4] = "Fishing Rod";
+     string [] enemies = new string [3];
+       enemies[0] = "Lizalfos"; // 
+       enemies[1] = "Moblin"; //
+       enemies[2] = "Bokoblin"; // Average
+     string [] armor = new string [4];
+       armor[0] = "Hylian"; // Good stats all around
+       armor[1] = "Sheikah"; // Good speed bad health
+       armor[2] = ""; //Good Health and Damage really bad speed
+       armor[3] = ""; // Good Attack Good speed mid health
+       
+  }
+}
+  
+
